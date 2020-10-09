06d11e4be6a1caf33e87c97d4ec597a8ed24b459
Misael de Castro Nazário <misael.nazario@demarco.com.br>
2020-10-08 23:23:07 -0300

    Incluir imagens, filtro e mostrar imagem

--- ProAgil-app/src/app/eventos/eventos.component.ts
+++ ProAgil-app/src/app/eventos/eventos.component.ts
@@ -7,11 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
-  eventos: any =[];
+
+  _filtroLista: string;
+  get filtroLista(): string{
+    return this._filtroLista;
+  }
+  set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  eventosFiltrados: any = [];
+  eventos: any = [];
+  imagemLargura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  // tslint:disable-next-line: typedef
+  alternarImagem(){
+    this.mostrarImagem = !this.mostrarImagem;
   }
 
   getEventos(){
@@ -20,8 +47,9 @@ export class EventosComponent implements OnInit {
         this.eventos = response;
         console.log(response);
       },
-      error => {console.log(error);}
-
+      error => {
+        console.log(error);
+      }
     );
   }
 
